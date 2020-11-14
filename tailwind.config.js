bbfa16ba3f57d9581a2f3cf95411ced5b5f167d1
CookieRamen <ark@arkjp.net>
2020-11-14 11:59:02 +0900

    Update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    enabled: enablePurge,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
   theme: {
     extend: {},
   },
